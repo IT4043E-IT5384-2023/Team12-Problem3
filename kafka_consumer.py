2ae32e9c9928819ac9bd9a9fa7198a3c8f91753f
phamthanhnam1302 <thanhnam1302pham@gmail.com>
2023-12-12 17:59:45 +0700

    update-kafka

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,11 +1,31 @@
 from kafka import KafkaConsumer
-import pydoop.hdfs as hdfs
-consumer = KafkaConsumer('testTopic',bootstrap_servers=['localhost:9092'])
-hdfs_path = 'hdfs://localhost:9000/TwitterData/data.json'
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+# Configure Kafka consumer
+KAFKA_URL = os.getenv("KAFKA_URL")
+KAFKA_TOPIC = os.getenv("KAFKA_ETHER_TOPIC")
+consumer = KafkaConsumer(KAFKA_TOPIC, bootstrap_servers=KAFKA_URL, group_id='tweet_consumer')
 
-for message in consumer:
-   values = message.value.decode('utf-8')
-   with hdfs.open(hdfs_path, 'at') as f:
-       print(message.value)
-       f.write(f"{values}\n")
+# Function to process and handle incoming tweets
+def process_tweet(tweet_json):
+    try:
+        tweet_data = json.loads(tweet_json)
+        # Add your processing logic here
+        print("Received tweet:", tweet_data)
+    except json.JSONDecodeError as e:
+        print("Error decoding JSON:", e)
+
+# Main script for consuming tweets
+if __name__ == "__main__":
+    try:
+        for message in consumer:
+            tweet_json = message.value.decode('utf-8')
+            process_tweet(tweet_json)
+    except KeyboardInterrupt:
+        print("Consumer interrupted. Closing.")
+    finally:
+        # Close Kafka consumer
+        consumer.close()
